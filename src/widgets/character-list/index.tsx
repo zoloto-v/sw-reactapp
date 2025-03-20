141fd1b3990824c95088a723aeb56cf95f1fccf0
Yura <generated.in@gmail.com>
2025-03-20 18:18:01 +0300

    remove unused components

--- src/widgets/character-list/index.tsx
+++
@@ -1,8 +0,0 @@
-import { CharacterPreview } from "../character-preview";
-import { ICharacter, IList } from "../../shared/types";
-
-export const CharacterList: React.FC<IList<ICharacter>> = ({ items = []}) => {
-  return items.map(item => (
-    <CharacterPreview {...item} />
-  ));
-};
